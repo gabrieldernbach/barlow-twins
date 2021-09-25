1afb706fd2d6b26ef4629728df36bd09f3758c72
gabriel <gabriel@aignostics.com>
2021-09-25 23:25:02 +0200

    init

---
+++ knn_test.py
@@ -0,0 +1,52 @@
+# This file is a copy from
+# https://github.com/yaohungt/Barlow-Twins-HSIC/blob/main/main.py
+
+import torch
+from tqdm import tqdm
+
+def test(net, db):
+    temperature = 0.5
+    k = 200
+    c = 10
+
+    net.eval()
+    total_top1, total_top5, total_num, feature_bank, target_bank = 0.0, 0.0, 0, [], []
+    with torch.no_grad():
+        # generate feature bank and target bank
+        for data, target in tqdm(db.memory, desc='Feature extracting'):
+            target_bank.append(target)
+            feature, out = net(data.cuda(non_blocking=True))
+            feature_bank.append(feature)
+        # [D, N]
+        feature_bank = torch.cat(feature_bank, dim=0).t().contiguous()
+        # [N]
+        feature_labels = torch.cat(target_bank, dim=0).contiguous().to(feature_bank.device)
+        # loop test data to predict the label by weighted knn search
+        test_bar = tqdm(db.test)
+        for data, target in test_bar:
+            data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
+            feature, out = net(data)
+
+            total_num += data.size(0)
+            # compute cos similarity between each feature vector and feature bank ---> [B, N]
+            sim_matrix = torch.mm(feature, feature_bank)
+            # [B, K]
+            sim_weight, sim_indices = sim_matrix.topk(k=k, dim=-1)
+            # [B, K]
+            sim_labels = torch.gather(feature_labels.expand(data.size(0), -1), dim=-1, index=sim_indices)
+            sim_weight = (sim_weight / temperature).exp()
+
+            # counts for each class
+            one_hot_label = torch.zeros(data.size(0) * k, c, device=sim_labels.device)
+            # [B*K, C]
+            one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
+            # weighted score ---> [B, C]
+            pred_scores = torch.sum(one_hot_label.view(data.size(0), -1, c) * sim_weight.unsqueeze(dim=-1), dim=1)
+
+            pred_labels = pred_scores.argsort(dim=-1, descending=True)
+            total_top1 += torch.sum((pred_labels[:, :1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
+            total_top5 += torch.sum((pred_labels[:, :5] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
+            test_bar.set_description('Test Epoch: Acc@1:{:.2f}% Acc@5:{:.2f}%'
+                                     .format(total_top1 / total_num * 100, total_top5 / total_num * 100))
+
+    return total_top1 / total_num * 100, total_top5 / total_num * 100
---
+++ main.py
@@ -0,0 +1,132 @@
+import torch
+import torch.backends.cudnn
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as T
+from torch.cuda.amp import GradScaler, autocast
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+from torchvision.models import resnet18
+from tqdm import tqdm
+
+from knn_test import test
+
+torch.backends.cudnn.benchmark = True
+
+
+class DoubleTransform:
+    def __init__(self):
+        self.tfm = T.Compose([
+            T.RandomResizedCrop(32),
+            T.RandomHorizontalFlip(p=0.5),
+            T.RandomApply([T.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            T.RandomGrayscale(p=0.2),
+            T.ToTensor(),
+            T.Normalize(
+                [0.4914, 0.4822, 0.4465],
+                [0.2023, 0.1994, 0.2010]
+            )
+        ])
+
+    def __call__(self, x):
+        return self.tfm(x), self.tfm(x)
+
+
+test_tfm = T.Compose([
+    T.ToTensor(),
+    T.Normalize(
+        [0.4914, 0.4822, 0.4465],
+        [0.2023, 0.1994, 0.2010]
+    )
+])
+
+
+class DataBunch:
+    train = DataLoader(
+        CIFAR10(root='data', train=True, download=True, transform=DoubleTransform()),
+        shuffle=True,
+        batch_size=128,
+        num_workers=16,
+        pin_memory=True,
+    )
+    memory = DataLoader(
+        CIFAR10(root='data', train=True, download=True, transform=test_tfm),
+        shuffle=True,
+        batch_size=128,
+        num_workers=16,
+        pin_memory=True,
+    )
+    test = DataLoader(
+        CIFAR10(root='data', train=False, download=True, transform=test_tfm),
+        shuffle=True,
+        batch_size=128,
+        num_workers=16,
+        pin_memory=True,
+    )
+
+
+class BarlowTwin(nn.Module):
+    def __init__(self, dim=128):
+        super(BarlowTwin, self).__init__()
+        self.encoder = resnet18()
+        # was conv 7x7 stride 2, but CIFAR images are small already
+        self.encoder.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False)
+        self.encoder.maxpool = nn.Identity()
+        self.encoder.fc = nn.Identity()
+
+        self.projector = nn.Sequential(
+            nn.Linear(512, 512, bias=False),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(512),
+            nn.Linear(512, dim, bias=True),
+        )
+
+    def forward(self, x):
+        feature = self.encoder(x)
+        out = self.projector(feature)
+        return F.normalize(feature, dim=-1), F.normalize(out, dim=-1)
+
+
+class BarlowLoss(nn.Module):
+    def __init__(self, llambda):
+        super(BarlowLoss, self).__init__()
+        self.llambda = llambda
+
+    def __call__(self, z_a, z_b):
+        z_a_norm = (z_a - z_a.mean(dim=0)) / z_a.std(dim=0)
+        z_b_norm = (z_b - z_b.mean(dim=0)) / z_b.std(dim=0)
+
+        n_batch, n_dim, = z_a.shape
+        c = torch.mm(z_a_norm.T, z_b_norm) / n_batch
+        c_diff = (c - torch.eye(n_dim, device="cuda")) ** 2
+        c_diff[~torch.eye(n_dim, dtype=torch.bool)] *= self.llambda
+        return c_diff.sum()
+
+
+if __name__ == "__main__":
+    model = BarlowTwin(dim=128).cuda()
+    criterion = BarlowLoss(llambda=0.005)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-3, weight_decay=1e-6)
+    scaler = GradScaler()
+
+    epochs = 1000
+    db = DataBunch()
+    for epoch in range(epochs):
+        loss_avg = 0.
+        batch_load = tqdm(db.train)
+        for idx, ((y_a, y_b), _) in enumerate(batch_load, start=1):
+            with autocast():
+                _, z_a = model(y_a.cuda(non_blocking=True))
+                _, z_b = model(y_b.cuda(non_blocking=True))
+                loss = criterion(z_a, z_b)
+
+            model.zero_grad(set_to_none=True)
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            loss_avg += (loss.item() - loss_avg) / idx
+            batch_load.set_description(f"{epoch:3d}, loss={loss_avg:.3f}")
+
+        if epoch % 5 == 0:
+            test_acc_1, test_acc_5 = test(model, db)
